7c6d650635813d35eaab61348e5548efe1ad4879
Pradeep Chauhan <pradeepchauhan91@gmail.com>
2021-01-11 15:02:48 +0530

    added type definition for graceful shutdown decorator

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,11 @@ export type fastifyGracefulShutdownOpt = { timeout?: number }
 
 export const fastifyGracefulShutdown: FastifyPluginCallback<fastifyGracefulShutdownOpt>
 export default fastifyGracefulShutdown
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    gracefulShutdown(
+      handler: (signal: string, next: (err?: Error) => void) => void
+    ): void
+  }
+}
